17d85f88c732bf1094c7d83f1f0faf8a25bff032
Mateusz <tynozobacz@gmail.com>
2019-07-30 21:37:33 +0200

    tasks

---
+++ week3-057.Words/src/Words.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Words {
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
+
+        System.out.println("Type a word: ");
+        String isEmpty = reader.nextLine();
+
+        while (!isEmpty.isEmpty()) {
+            words.add(isEmpty);
+
+            System.out.println("Type a word: ");
+            isEmpty = reader.nextLine();
+        }
+
+        System.out.println("You typed the following words: ");
+        for (String word : words) {
+            System.out.println(word);
+        }
+
+    }
+}
+
+/*
+MODEL_SOLUTION
+public class Words {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<String>();
+        while (true) {
+            System.out.print("Type a word: ");
+            String word = reader.nextLine();
+            if (word.equals("")) {
+                break;
+            }
+            words.add(word);
+        }
+
+        System.out.println("You typed the following words:");
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
+
+
+ */
